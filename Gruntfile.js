31bf7aedbf858803c694024ba334aba64765da7e
Ville Pulkkinen <ville.p.pulkkinen@gmail.com>
2015-01-03 13:16:43 +0200

    fixed indendation

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,6 +17,7 @@ module.exports = function(grunt){
 			options: {
 				banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
 			},
+		
 			dist: {
 				files: {
 					'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
